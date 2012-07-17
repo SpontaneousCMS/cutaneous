47cc5b6ee4e76cfbadac2f4b836608eb7fcb8f05
Garry Hill <garry@magnetised.info>
2012-07-17 13:23:57 +0100

    Rename test_helper to avoid running it as a test

--- test/test_blocks.rb
+++ test/test_blocks.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../test_helper', __FILE__)
+require File.expand_path('../helper', __FILE__)
 
 describe Cutaneous do
   let(:template_root) { File.expand_path("../fixtures", __FILE__)                     }
