88d0e5a166c72ba66600b76c50e885a9681c6590
Garry Hill <garry@magnetised.info>
2012-07-18 15:33:23 +0100

    Remove mysterious duplicate line. Who did that?

--- test/test_core.rb
+++ test/test_core.rb
@@ -3,7 +3,6 @@ require File.expand_path('../helper', __FILE__)
 describe Cutaneous do
   let(:template_root) { File.expand_path("../fixtures", __FILE__)                     }
   let(:engine)        { Cutaneous::Engine.new(template_root, Cutaneous::FirstPassLexer, "html") }
-  let(:engine)        { Cutaneous::Engine.new(template_root, Cutaneous::FirstPassLexer, "html") }
 
   it "Will parse & execute a simple template with expressions" do
     context = ContextHash(right: "right", code: "<tag/>")
